0f6cb6f1013f6a1649307dbc5eb126ee6ed73c03
badman99dev <badman993944@gmail.com>
2025-10-28 00:17:34 +0530

    Update tool_manager.py

--- tools/tool_manager.py
+++ tools/tool_manager.py
@@ -1,7 +1,12 @@
+# tools/tool_manager.py
+
 from .youtube_transcript import fetch_youtube_details_from_api
-from .quiz_tool import send_quiz_poll # Naya tool
+from .quiz_tool import send_quiz_poll
+from .movie_tools import search_movie_in_database, get_details_and_download_links
 
 AVAILABLE_TOOLS = [
     fetch_youtube_details_from_api,
-    send_quiz_poll, # Naya tool
+    send_quiz_poll,
+    search_movie_in_database,
+    get_details_and_download_links,
 ]
