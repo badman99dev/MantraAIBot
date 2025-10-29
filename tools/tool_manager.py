551411ae22fa60de40b921d906ea91820018beee
badman99dev <badman993944@gmail.com>
2025-10-29 13:30:12 +0530

    Update tool_manager.py

--- tools/tool_manager.py
+++ tools/tool_manager.py
@@ -1,12 +1,12 @@
 # tools/tool_manager.py
 
 from .youtube_transcript import fetch_youtube_details_from_api
-from .quiz_tool import send_quiz_poll
+from .quiz_tool import manage_quiz # <-- BADLAAV YAHAN HAI
 from .movie_tools import search_movie_in_database, get_details_and_download_links
 
 AVAILABLE_TOOLS = [
     fetch_youtube_details_from_api,
-    send_quiz_poll,
+    manage_quiz, # <-- AUR YAHAN HAI
     search_movie_in_database,
     get_details_and_download_links,
 ]
